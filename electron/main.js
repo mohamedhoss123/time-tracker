d47d85ad714eff2e70364b46e5a2b691f63f9dc2
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2023-02-10 09:37:23 +0200

    add ping

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow , ipcMain} = require('electron');
 const path = require("path")
 function createWindow() {
     const win = new BrowserWindow({
@@ -31,3 +31,9 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+
+
+ipcMain.on("ping",() => {
+    console.log("ping!");
+})
